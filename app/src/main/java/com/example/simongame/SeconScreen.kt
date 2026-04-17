effecdf2924b49b5186937b1068bc4b34721f8a6
CrazyMemone <emanueleconton@MacBookAir.station>
2026-04-17 16:56:55 +0200

    configurazione portrait e landscape per la seconda schermata e aggiunte dettagli grafici

--- app/src/main/java/com/example/simongame/SeconScreen.kt
+++ app/src/main/java/com/example/simongame/SeconScreen.kt
@@ -1,45 +1,106 @@
 package com.example.simongame
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SeconScreen(rounds: List<Round>) {
-    LazyColumn {
-        items(rounds) { round ->
-            RoundItem(round = round)
+    val orientation = LocalConfiguration.current.orientation
+    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        // landscape
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 64.dp)
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+            items(rounds) { round ->
+                RoundItem(round = round)
+            }
+        }
+    } else {
+        // portrait
+        LazyColumn(
+            modifier = Modifier
+                .padding(horizontal = 0.dp) // tutto lo spazio orizzontale disponibile
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(90.dp))
+            }
+            items(rounds) { round ->
+                RoundItem(round = round)
+            }
         }
     }
 }
 
-// Singolo elemento della lista
+//singolo elemento della lista
 @Composable
 fun RoundItem(round: Round) {
     Row(
         modifier = Modifier
-            .fillMaxWidth()
             .padding(16.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        // numero colori premuti a sinistra
-        Text(
-            text = round.getCount().toString(),
-            modifier = Modifier.padding(end = 30.dp)
-        )
-        // sequenza troncata a destra
-        Text(
-            text = round.printSequence(),
-            maxLines = 1,
-            overflow = Ellipsis
-        )
+        // cerchio per il numero di elementi
+        Surface(
+            color = MaterialTheme.colorScheme.primaryContainer,
+            shape = MaterialTheme.shapes.medium,
+            modifier = Modifier.size(45.dp)
+        ) {
+            Box(contentAlignment = Alignment.Center) {
+                Text(
+                    text = round.getCount().toString(),
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.DarkGray,
+                    lineHeight = 30.sp
+                )
+            }
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = round.printSequence(),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis, // indicatore grafico del troncamento (...)
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.DarkGray,
+                lineHeight = 30.sp
+            )
+        }
     }
+
 }
